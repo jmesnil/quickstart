75d827711af56cb81710eca5d7d97a3ea58a9ecd
Jeff Mesnil <jmesnil@redhat.com>
2020-02-18 20:58:24 +0100

    use @ApplicationPath()

--- microprofile-metrics/src/main/java/org/wildfly/quickstart/microprofile/metrics/JaxRsApplication.java
+++ microprofile-metrics/src/main/java/org/wildfly/quickstart/microprofile/metrics/JaxRsApplication.java
@@ -3,6 +3,6 @@ package org.wildfly.quickstart.microprofile.metrics;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/")
+@ApplicationPath("")
 public class JaxRsApplication extends Application {
 }
